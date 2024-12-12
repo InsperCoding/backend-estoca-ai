1cdae976e93eb0ed0a2425f09ab692816648fb61
Thiago Penha <thiagoppc@al.insper.edu.br>
2024-12-12 14:20:13 -0300

    login funcionando com o token

---
+++ estocaai-backend/src/main/java/com/estocaai/backend/User/Model/User.java
@@ -0,0 +1,53 @@
+package com.estocaai.backend.User.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private String id;
+
+    private String email;
+
+    private String password;
+
+    private String token;
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
